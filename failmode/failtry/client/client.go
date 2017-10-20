2e9d6dcf55794ab7f150274ac8438debfd679ed9
smallnest <smallnest@gmail.com>
2017-10-20 18:06:38 +0800

    add failmode examples

---
+++ failmode/failtry/client/client.go
@@ -0,0 +1,42 @@
+// go run -tags zookeeper client.go
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr1 = flag.String("addr1", "localhost:8972", "server1 address")
+	addr2 = flag.String("addr2", "localhost:9981", "server2 address")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewMultipleServersDiscovery([]*client.KVPair{{Key: *addr1}, {Key: *addr2}})
+	option := client.DefaultOption
+	option.Retries = 10
+	xclient := client.NewXClient("Arith", "Mul", client.Failtry, client.RandomSelect, d, option)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for i := 0; i < 10; i++ {
+		reply := &example.Reply{}
+		err := xclient.Call(context.Background(), args, reply, nil)
+		if err != nil {
+			log.Printf("failed to call: %v", err)
+		} else {
+			log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+		}
+
+	}
+}
---
+++ failmode/failtry/server.go
@@ -0,0 +1,42 @@
+// go run -tags zookeeper server.go
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr1 = flag.String("addr1", "localhost:8972", "server1 address")
+	addr2 = flag.String("addr2", "localhost:9981", "server2 address")
+)
+
+type Arith int
+
+func (t *Arith) Mul(ctx context.Context, args *example.Args, reply *example.Reply) error {
+	*t = *t + 1
+	if *t%2 == 0 {
+		return errors.New("unknown error")
+	}
+	reply.C = args.A * args.B
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	go createServer(*addr1)
+	go createServer(*addr2)
+
+	select {}
+}
+
+func createServer(addr string) {
+	s := server.NewServer(nil)
+	s.RegisterName("Arith", new(Arith), "")
+	s.Serve("tcp", addr)
+}
