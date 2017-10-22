f472b92fa16f0f2358dd4e6250c9125775151a40
smallnest <smallnest@gmail.com>
2017-10-22 19:50:28 +0800

    add heartbeat example

---
+++ heartbeat/client/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
+
+	option := client.DefaultOption
+	option.Heartbeat = true
+	option.HeartbeatInterval = time.Second
+
+	xclient := client.NewXClient("Arith", "Mul", client.Failtry, client.RandomSelect, d, option)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for i := 0; i < 10; i++ {
+		reply := &example.Reply{}
+		err := xclient.Call(context.Background(), args, reply, nil)
+		if err != nil {
+			log.Fatalf("failed to call: %v", err)
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+		time.Sleep(time.Minute)
+	}
+
+}
