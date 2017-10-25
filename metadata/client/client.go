4f35c29f16b73d0787d852a3b940443625632cd9
smallnest <smallnest@gmail.com>
2017-10-25 23:40:28 +0800

    add meta examples

---
+++ metadata/client/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"github.com/smallnest/rpcx/share"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
+
+	option := client.DefaultOption
+	option.ReadTimeout = 10 * time.Second
+
+	xclient := client.NewXClient("Arith", "Mul", client.Failtry, client.RandomSelect, d, option)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &example.Reply{}
+	ctx := context.WithValue(context.Background(), share.ReqMetaDataKey, map[string]string{"aaa": "from client"})
+	ctx = context.WithValue(ctx, share.ResMetaDataKey, make(map[string]string))
+	err := xclient.Call(ctx, args, reply, nil)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+	log.Printf("received meta: %+v", ctx.Value(share.ResMetaDataKey))
+}
---
+++ metadata/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"github.com/smallnest/rpcx/share"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+type Arith int
+
+func (t *Arith) Mul(ctx context.Context, args *example.Args, reply *example.Reply) error {
+	reqMeta := ctx.Value(share.ReqMetaDataKey).(map[string]string)
+	resMeta := ctx.Value(share.ResMetaDataKey).(map[string]string)
+
+	fmt.Printf("received meta: %+v\n", reqMeta)
+
+	resMeta["echo"] = "from server"
+
+	reply.C = args.A * args.B
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	s := server.Server{}
+	s.RegisterName("Arith", new(Arith), "")
+	s.Serve("tcp", *addr)
+}
