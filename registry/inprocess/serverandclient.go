b9471808bb03feb3fff4e405bcce5972eb2b755c
smallnest <smallnest@gmail.com>
2017-10-25 12:25:30 +0800

    add in-process examples

---
+++ registry/inprocess/serverandclient.go
@@ -0,0 +1,58 @@
+// go run -tags etcd server.go
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr     = flag.String("addr", "localhost:8972", "server address")
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer(nil)
+	addRegistryPlugin(s)
+
+	s.RegisterName("Arith", new(example.Arith), "")
+
+	go func() {
+		s.Serve("tcp", *addr)
+	}()
+
+	d := client.NewInpreocessDiscovery()
+	xclient := client.NewXClient("Arith", "Mul", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for i := 0; i < 100; i++ {
+
+		reply := &example.Reply{}
+		err := xclient.Call(context.Background(), args, reply, nil)
+		if err != nil {
+			log.Fatalf("failed to call: %v", err)
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+	}
+}
+
+func addRegistryPlugin(s *server.Server) {
+
+	r := client.InprocessClient
+	s.Plugins.Add(r)
+}
