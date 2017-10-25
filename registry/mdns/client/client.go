a2f6c3cd9b358c21b86a2e62f434ae98c7614a14
smallnest <smallnest@gmail.com>
2017-10-25 08:31:49 +0800

    add mdns examaple

---
+++ registry/mdns/client/client.go
@@ -0,0 +1,39 @@
+// go run -tags etcd client.go
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test/Arith", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewMDNSDiscovery("Arith", 10*time.Second, 10*time.Second, "")
+	xclient := client.NewXClient("Arith", "Mul", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &example.Reply{}
+	err := xclient.Call(context.Background(), args, reply, nil)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+}
---
+++ registry/mdns/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	metrics "github.com/rcrowley/go-metrics"
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+)
+
+var (
+	addr     = flag.String("addr", "localhost:8972", "server address")
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer(nil)
+	addRegistryPlugin(s)
+
+	s.RegisterName("Arith", new(example.Arith), "")
+	s.Serve("tcp", *addr)
+}
+
+func addRegistryPlugin(s *server.Server) {
+
+	r := serverplugin.NewMDNSRegisterPlugin("tcp@"+*addr, 8972, metrics.NewRegistry(), time.Minute, "")
+	err := r.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Plugins.Add(r)
+}
