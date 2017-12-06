d0901287b2b9f55a8c88de8620c0d50c1b96169b
smallnest <smallnest@gmail.com>
2017-12-06 14:32:07 +0800

    add customized codec

---
+++ codec/gob/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"flag"
+	"fmt"
+
+	"github.com/smallnest/rpcx/protocol"
+	"github.com/smallnest/rpcx/share"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	share.Codecs[protocol.SerializeType(4)] = &GobCodec{}
+	s := server.NewServer()
+	//s.RegisterName("Arith", new(example.Arith), "")
+	s.Register(new(example.Arith), "")
+	s.Serve("tcp", *addr)
+}
+
+type GobCodec struct {
+}
+
+func (c *GobCodec) Decode(data []byte, i interface{}) error {)
+	enc := gob.NewDecoder(bytes.NewBuffer(data))
+	err := enc.Decode(i)
+	return err
+}
+
+func (c *GobCodec) Encode(i interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(i)
+	return buf.Bytes(), err
+}
