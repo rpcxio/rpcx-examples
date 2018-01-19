e14fcbd41b7e89d4beacdb4a110bc6720093cee4
smallnest <smallnest@gmail.com>
2018-01-19 18:25:45 +0800

    #183 invoke rpcx services via http

---
+++ http_invoke/client/httpclient.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/rpcx-ecosystem/rpcx-gateway"
+
+	"github.com/smallnest/rpcx/codec"
+)
+
+type Args struct {
+	A int
+	B int
+}
+
+type Reply struct {
+	C int
+}
+
+func main() {
+	cc := &codec.MsgpackCodec{}
+
+	args := &Args{
+		A: 10,
+		B: 20,
+	}
+
+	data, _ := cc.Encode(args)
+
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8972/", bytes.NewReader(data))
+	if err != nil {
+		log.Fatal("failed to create request: ", err)
+		return
+	}
+
+	h := req.Header
+	h.Set(gateway.XMessageID, "10000")
+	h.Set(gateway.XMessageType, "0")
+	h.Set(gateway.XSerializeType, "3")
+	h.Set(gateway.XServicePath, "Arith")
+	h.Set(gateway.XServiceMethod, "Mul")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("failed to call: ", err)
+	}
+	defer res.Body.Close()
+
+	// handle http response
+	replyData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("failed to read response: ", err)
+	}
+
+	reply := &Reply{}
+	err = cc.Decode(replyData, reply)
+	if err != nil {
+		log.Fatal("failed to decode reply: ", err)
+	}
+
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+}
---
+++ http_invoke/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+	s.Register(new(example.Arith), "")
+	s.Serve("tcp", *addr)
+}
