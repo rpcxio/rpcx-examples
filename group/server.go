5dc4ea88c2130eee4195a08fdc13a1a49ef5f1aa
smallnest <smallnest@gmail.com>
2018-02-02 12:54:56 +0800

    add state & group examples

---
+++ group/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr1 = flag.String("addr1", "localhost:8972", "server1 address")
+	addr2 = flag.String("addr2", "localhost:9981", "server2 address")
+)
+
+type Arith int
+
+func (t *Arith) Mul(ctx context.Context, args *example.Args, reply *example.Reply) error {
+	reply.C = args.A * args.B * 100
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	go createServer1(*addr1, "")
+	go createServer2(*addr2, "group=test")
+
+	select {}
+}
+
+func createServer1(addr, meta string) {
+	s := server.NewServer()
+	s.RegisterName("Arith", new(example.Arith), meta)
+	s.Serve("tcp", addr)
+}
+
+func createServer2(addr, meta string) {
+	s := server.NewServer()
+	s.RegisterName("Arith", new(Arith), meta)
+	s.Serve("tcp", addr)
+}
