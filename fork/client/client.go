a506f9e48679b4ba227f964becc24e36cfb0c4d0
smallnest <smallnest@gmail.com>
2018-02-05 13:17:41 +0800

    add fork/broadcast examples

---
+++ fork/client/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr1 = flag.String("addr1", "tcp@localhost:8972", "server1 address")
+	addr2 = flag.String("addr2", "tcp@localhost:9981", "server2 address")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewMultipleServersDiscovery([]*client.KVPair{{Key: *addr1}, {Key: *addr2}})
+	xclient := client.NewXClient("Arith", client.Failover, client.RoundRobin, d, client.DefaultOption)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for {
+		reply := &example.Reply{}
+		err := xclient.Fork(context.Background(), "Mul", args, reply)
+		if err != nil {
+			log.Fatalf("failed to call: %v", err)
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+		time.Sleep(1e9)
+	}
+
+}
