f48b903bab327b1fb67d35f81b232c1cc31ad740
smallnest <smallnest@gmail.com>
2018-04-20 18:35:21 +0800

    add server log

--- service.go
+++ service.go
@@ -1,7 +1,10 @@
 // Package example defines datastructure and services.
 package example
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type Args struct {
 	A int
@@ -16,10 +19,12 @@ type Arith int
 
 func (t *Arith) Mul(ctx context.Context, args *Args, reply *Reply) error {
 	reply.C = args.A * args.B
+	fmt.Printf("call: %d * %d = %d\n", args.A, args.B, reply.C)
 	return nil
 }
 
 func (t *Arith) Add(ctx context.Context, args *Args, reply *Reply) error {
 	reply.C = args.A + args.B
+	fmt.Printf("call: %d + %d = %d\n", args.A, args.B, reply.C)
 	return nil
 }
