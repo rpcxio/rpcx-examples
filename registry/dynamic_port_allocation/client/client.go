464edc4c07666fbe114cbb32678f799eb6f481b4
smallnest <smallnest@gmail.com>
2018-09-13 20:04:56 +0800

    add dynamic port allocation example

---
+++ registry/dynamic_port_allocation/client/client.go
@@ -0,0 +1,45 @@
+// go run -tags etcd client.go
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewEtcdDiscovery(*basePath, "Arith", []string{*etcdAddr}, nil)
+	xclient := client.NewXClient("Arith", client.Failover, client.RoundRobin, d, client.DefaultOption)
+	defer xclient.Close()
+
+	args := &example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for {
+		reply := &example.Reply{}
+		err := xclient.Call(context.Background(), "Mul", args, reply)
+		if err != nil {
+			log.Printf("failed to call: %v\n", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+		time.Sleep(5 * time.Second)
+	}
+
+}
---
+++ registry/dynamic_port_allocation/server.go
@@ -0,0 +1,50 @@
+// go run -tags etcd server.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	metrics "github.com/rcrowley/go-metrics"
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+)
+
+var (
+	addr     = flag.String("addr", "localhost:0", "server address")
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+
+	go s.Serve("tcp", *addr)
+
+	time.Sleep(2 * time.Second)
+	*addr = s.Address().String()
+	fmt.Println("listened on: ", *addr)
+
+	r := createRegistryPlugin()
+	s.Plugins.Add(r)
+	s.RegisterName("Arith", new(example.Arith), "")
+	r.Start()
+
+	select {}
+}
+func createRegistryPlugin() *serverplugin.EtcdRegisterPlugin {
+
+	r := &serverplugin.EtcdRegisterPlugin{
+		ServiceAddress: "tcp@" + *addr,
+		EtcdServers:    []string{*etcdAddr},
+		BasePath:       *basePath,
+		Metrics:        metrics.NewRegistry(),
+		UpdateInterval: time.Minute,
+	}
+
+	return r
+}
