34a1ac872c7988dd0179dbbf5bcafd2cd81e1d6e
smallnest <smallnest@gmail.com>
2019-03-27 20:29:50 +0800

    add pool example

---
+++ pool/client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
+
+	pool := client.NewXClientPool(10, "Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer pool.Close()
+
+	args := example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for i := 0; i < 100; i++ {
+		reply := &example.Reply{}
+		xclient := pool.Get()
+		err := xclient.Call(context.Background(), "Mul", args, reply)
+		if err != nil {
+			log.Fatalf("failed to call: %v", err)
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+	}
+}
