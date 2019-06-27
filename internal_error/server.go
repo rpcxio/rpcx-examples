c188fc5068a78d2e3a4e553c26f141ca57317adb
smallnest <smallnest@gmail.com>
2019-06-27 18:00:19 +0800

    add internal_error example

---
+++ internal_error/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	example "github.com/rpcx-ecosystem/rpcx-examples3"
+	"github.com/smallnest/rpcx/server"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+type Arith struct{}
+
+// the second parameter is not a pointer
+func (t *Arith) Mul(ctx context.Context, args example.Args, reply *example.Reply) error {
+	reply.C = args.A * args.B
+	var m map[string]string
+	m["error"] = "nil map error"
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+	//s.Register(new(Arith), "")
+	s.RegisterName("Arith", new(Arith), "")
+	err := s.Serve("tcp", *addr)
+	if err != nil {
+		panic(err)
+	}
+}
