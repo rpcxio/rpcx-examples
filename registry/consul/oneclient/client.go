c31e4ea92339a3fceccc7d00da6105eb6ef01085
smallnest <smallnest@gmail.com>
2019-07-20 21:39:49 +0800

    add OneClient example for consul

--- registry/consul/oneclient/client.go
+++ registry/consul/oneclient/client.go
@@ -18,24 +18,24 @@ var (
 func main() {
 	flag.Parse()
 
-	d := client.NewConsulDiscovery(*basePath, "Arith", []string{*consulAddr}, nil)
-	xclient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
-	defer xclient.Close()
+	d := client.NewConsulDiscoveryTemplate(*basePath, []string{*consulAddr}, nil)
+	oneClient := client.NewOneClient(client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer oneClient.Close()
 
 	args := &example.Args{
 		A: 10,
 		B: 20,
 	}
 
+	reply := &example.Reply{}
 	for {
-		reply := &example.Reply{}
-		err := xclient.Call(context.Background(), "Mul", args, reply)
+		err := oneClient.Call(context.Background(), "Arith", "Mul", args, reply)
 		if err != nil {
-			log.Printf("ERROR failed to call: %v", err)
+			log.Fatalf("failed to call: %v", err)
 		}
 
 		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
-		time.Sleep(1e9)
+		time.Sleep(time.Second)
 	}
 
 }
