f0e6c446807a4134946e9f81f41fd2b1cee0bfea
smallnest <smallnest@gmail.com>
2019-11-27 18:22:40 +0800

    update quic

--- quic/server.go
+++ quic/server.go
@@ -2,9 +2,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"flag"
-	"log"
+	"fmt"
+	"math/big"
+	"time"
 
 	example "github.com/rpcx-ecosystem/rpcx-examples3"
 	"github.com/smallnest/rpcx/server"
@@ -17,13 +23,18 @@ var (
 func main() {
 	flag.Parse()
 
-	cert, err := tls.LoadX509KeyPair("server.pem", "server.key")
-	if err != nil {
-		log.Print(err)
-		return
-	}
+	// cert, err := tls.LoadX509KeyPair("server.pem", "server.key")
+	// if err != nil {
+	// 	log.Print(err)
+	// 	return
+	// }
 
-	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+	// config := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	config, err := generateTLSConfig()
+	if err != nil {
+		panic(err)
+	}
 
 	s := server.NewServer(server.WithTLSConfig(config))
 	s.RegisterName("Arith", new(example.Arith), "")
@@ -32,4 +43,36 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func generateTLSConfig() (*tls.Config, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(time.Hour),
+		KeyUsage:     x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key),
+	})
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+	certPEM := pem.EncodeToMemory(&b)
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+	}, nil
 }
