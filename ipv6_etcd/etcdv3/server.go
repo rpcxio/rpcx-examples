bcf25ee7f75479a3e49ecc2e032d228923dcee3d
smallnest <smallnest@gmail.com>
2020-08-11 19:30:59 +0800

    add ipv6 registry example

---
+++ ipv6_etcd/etcdv3/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+
+	example "github.com/rpcxio/rpcx-examples"
+	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+)
+
+var (
+	addr     = flag.String("addr", "[::1]:8972", "server address")
+	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	go http.ListenAndServe(":9981", nil)
+
+	s := server.NewServer()
+	addRegistryPlugin(s)
+
+	s.RegisterName("Arith", new(example.Arith), "")
+	err := s.Serve("tcp", *addr)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func addRegistryPlugin(s *server.Server) {
+
+	r := &serverplugin.EtcdV3RegisterPlugin{
+		ServiceAddress: "tcp@" + *addr,
+		EtcdServers:    []string{*etcdAddr},
+		BasePath:       *basePath,
+		UpdateInterval: time.Minute,
+	}
+	err := r.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Plugins.Add(r)
+}
