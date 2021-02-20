013e1d9be2d65c2eabfe9f8e93cf946682e79130
chaoyuepan <chaoyuepan@baidu.com>
2021-02-20 16:55:06 +0800

    add nil_reply examples

---
+++ nil_reply_server/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/smallnest/rpcx/protocol"
+
+	example "github.com/rpcxio/rpcx-examples"
+	"github.com/smallnest/rpcx/client"
+)
+
+var addr = flag.String("addr", "localhost:8972", "server address")
+
+func main() {
+	flag.Parse()
+
+	d, _ := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
+	opt := client.DefaultOption
+	opt.SerializeType = protocol.JSON
+
+	xclient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, opt)
+	defer xclient.Close()
+
+	args := example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	err := xclient.Call(context.Background(), "Mul", args, nil)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("%d * %d = nil", args.A, args.B)
+}
