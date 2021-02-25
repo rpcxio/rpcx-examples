1e63e3b91d15a10d62c0a00d21c222a08948bb51
smallnest <smallnest@gmail.com>
2021-02-25 09:32:19 +0800

    upgrade rpcx

--- registry/dynamic_port_allocation/go.mod
+++ registry/dynamic_port_allocation/go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
 	github.com/rpcxio/rpcx-etcd v0.0.0-20201229103411-8317fc934fbb
 	github.com/rpcxio/rpcx-examples v1.1.6
-	github.com/smallnest/rpcx v0.0.0-20210219130000-031af6a9dc4c
+	github.com/smallnest/rpcx v0.0.0-20210225012836-75a1fadd8302
 	google.golang.org/grpc v1.34.0 // indirect
 )
 
--- upgrade.sh
+++ upgrade.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 
-go get -u -v github.com/smallnest/rpcx
+go get -u -v github.com/smallnest/rpcx@master
 
 cd registry/dynamic_port_allocation
-go get -u -v github.com/smallnest/rpcx
+go get -u -v github.com/smallnest/rpcx@master
 
 cd ../../registry/etcd
-go get -u -v github.com/smallnest/rpcx
+go get -u -v github.com/smallnest/rpcx@master
 
 cd ../../registry/etcdv3
-go get -u -v github.com/smallnest/rpcx
+go get -u -v github.com/smallnest/rpcx@master
 
 cd ../../registry/etcdv3_lazyregister
-go get -u -v github.com/smallnest/rpcx
+go get -u -v github.com/smallnest/rpcx@master
 
 cd ../..
