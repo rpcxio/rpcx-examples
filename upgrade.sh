d3d93c35c255afb703cb47fcb9726dfbdb5b9e08
chaoyuepan <chaoyuepan@baidu.com>
2021-03-12 14:22:28 +0800

    #561 fix wrong deadline

--- upgrade.sh
+++ upgrade.sh
@@ -1,17 +1,19 @@
 #! /bin/sh
 
-go get -u -v github.com/smallnest/rpcx@master
+version=a54ce6558688ec7cb9bdbebf457151bcf634e874
+
+go get -v github.com/smallnest/rpcx@$version
 
 cd registry/dynamic_port_allocation
-go get -u -v github.com/smallnest/rpcx@master
+go get -v github.com/smallnest/rpcx@$version
 
 cd ../../registry/etcd
-go get -u -v github.com/smallnest/rpcx@master
+go get -v github.com/smallnest/rpcx@$version
 
 cd ../../registry/etcdv3
-go get -u -v github.com/smallnest/rpcx@master
+go get -v github.com/smallnest/rpcx@$version
 
 cd ../../registry/etcdv3_lazyregister
-go get -u -v github.com/smallnest/rpcx@master
+go get -v github.com/smallnest/rpcx@$version
 
 cd ../..
