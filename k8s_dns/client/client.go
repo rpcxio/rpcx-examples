4dd16fed5d6a796b6cdecbe355bd7dd4c3a579e1
chaoyuepan <chaoyuepan@baidu.com>
2022-02-09 19:44:04 +0800

    add k8s based-dns examples

---
+++ k8s_dns/client/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	example "github.com/rpcxio/rpcx-examples"
+	"github.com/smallnest/rpcx/client"
+)
+
+func main() {
+	d, _ := client.NewDNSDiscovery("rpcx-server-demo-service", "tcp", 8972, time.Minute)
+	xclient := client.NewXClient("Arith", client.Failover, client.RoundRobin, d, client.DefaultOption)
+	defer xclient.Close()
+	args := example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	for {
+		reply := &example.Reply{}
+		err := xclient.Call(context.Background(), "Mul", args, reply)
+		if err != nil {
+			log.Fatalf("failed to call: %v", err)
+		}
+
+		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+		time.Sleep(time.Second)
+	}
+}
