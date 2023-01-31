14153bd6e2e770846a5e25d9c38c4aaa8e1d2162
chaoyuepan <chaoyuepan@baidu.com>
2023-01-31 14:06:37 +0800

    add blacklist

---
+++ plugin_blacklist/client/client.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+
+	example "github.com/rpcxio/rpcx-examples"
+	"github.com/smallnest/rpcx/client"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+var (
+	xclient client.XClient
+)
+
+func main() {
+	flag.Parse()
+
+	go http.ListenAndServe(":9099", nil)
+	d, _ := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
+	xclient = client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	plugins := client.NewPluginContainer()
+	plugins.Add(&ConnectionPlugin{})
+	xclient.SetPlugins(plugins)
+
+	args := example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &example.Reply{}
+	err := xclient.Call(context.Background(), "Mul", args, reply)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+}
+
+type ConnectionPlugin struct {
+}
+
+func (p *ConnectionPlugin) ClientConnected(conn net.Conn) (net.Conn, error) {
+	log.Printf("server %v connected", conn.RemoteAddr().String())
+
+	args := example.Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &example.Reply{}
+	err := xclient.Call(context.Background(), "Mul", args, reply)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+
+	return conn, nil
+}
+
+func (p *ConnectionPlugin) ClientConnectionClose(conn net.Conn) error {
+	log.Printf("server %v closed", conn.RemoteAddr().String())
+	return nil
+}
---
+++ plugin_blacklist/server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	example "github.com/rpcxio/rpcx-examples"
+	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+type Arith struct{}
+
+// the second parameter is not a pointer
+func (t *Arith) Mul(ctx context.Context, args example.Args, reply *example.Reply) error {
+	reply.C = args.A * args.B
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+	s.Plugins.Add(&serverplugin.BlacklistPlugin{
+		Blacklist: map[string]bool{
+			"127.0.0.1": true,
+		},
+	})
+
+	//s.Register(new(Arith), "")
+	s.RegisterName("Arith", new(Arith), "")
+	err := s.Serve("tcp", *addr)
+	if err != nil {
+		panic(err)
+	}
+}
